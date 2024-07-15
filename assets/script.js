b9a10b1a2415246b18f7bb7241e5bb0319d6dca7
theMayo-coder <maya.dudhyala@gmail.com>
2024-07-15 02:16:24 -0500

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -34,6 +34,8 @@ function setClientWidth() {
     );
     var s = htmlElement.style.getPropertyValue("--client-width");
     console.log(s);
+
+    console.log("transform property", $('#sai-baba-img').css("transform"));
 }
 
 setScrollVar();
@@ -45,25 +47,23 @@ setClientWidth();
 //creating functionality for each of the a
 $(document).ready(function() {
 
+    console.log("page is ready");
+
     //home button link
     $('.home-button').on('click', function() {
-        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/');
+        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/index.html');
     });
 
     $('.about-us-button').on('click', function() {
-        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/about_us.html');
+        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/about_us_copy.html');
     });
 
     $('.temple-services-button').on('click', function() {
-        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/temple_services.html');
+        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/temple_services_copy.html');
     });
 
     $('.events-button').on('click', function() {
-        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/events.html');
-    });
-
-    $('.contact-us-button').on('click', function() {
-        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/contact_us.html');
+        window.location.href = window.location.href.replace(/\/[^\/]*$/, '/events_copy.html');
     });
 
 });
